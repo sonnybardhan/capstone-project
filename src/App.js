4d0a2681f09e1e0694111b6b06f35783132bca62
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-23 15:22:51 +0530

    create basic components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
 function App() {
-  return <div>hello world</div>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      img: '',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      img: '',
+    },
+    {
+      id: 3,
+      title: 'Sneeakers',
+      img: '',
+    },
+    {
+      id: 4,
+      title: 'Mens',
+      img: '',
+    },
+    {
+      id: 5,
+      title: 'Womens',
+      img: '',
+    },
+  ];
+
+  return (
+    <div className='categories-container'>
+      {categories.map(({ id, title }) => (
+        <CategoryComponent key={id} title={title} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
+
+const CategoryComponent = ({ img, title }) => {
+  return (
+    <div className='category-container'>
+      <div className='background-image'></div>
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
