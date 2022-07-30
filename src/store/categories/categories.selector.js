5c4969f4b325b9bb23cdc432b97624bfe0f2d468
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-30 15:26:36 +0530

    modify categories selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,43 +1,16 @@
 import { createSelector } from 'reselect';
 
-const selectCategoriesReducer = (state) => {
-  // console.log('selector 1 ran');
-  // console.log('1. state.categories: ', state.categories);
-  return state.categories;
-};
+const selectCategoriesReducer = (state) => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoriesReducer],
-  (categoriesSlice) => {
-    // console.log('selector 2 ran');
-    // console.log('2. categoriesSlice.categories: ', categoriesSlice.categories);
-    return categoriesSlice.categories;
-  }
+  (categoriesSlice) => categoriesSlice.categories
 );
 
-export const selectCategory = createSelector(
-  [selectCategories],
-  (categories) => {
-    // console.log('selector 3 ran');
-    const result = categories.reduce((acc, category) => {
-      const { title, items } = category;
-      acc[title.toLowerCase()] = items;
-      return acc;
-    }, {});
-    // console.log('3. categories: ', result);
-    return result;
-  }
+export const selectCategory = createSelector([selectCategories], (categories) =>
+  categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
 );
-// export const selectCategory = createSelector(
-//   [selectCategories],
-//   (categories) => {
-//     console.log('selector 3 ran');
-//     const result = categories.reduce((acc, category) => {
-//       const { title, items } = category;
-//       acc[title.toLowerCase()] = items;
-//       return acc;
-//     }, {});
-//     console.log('3. categories: ', result);
-//     return result;
-//   }
-// );
